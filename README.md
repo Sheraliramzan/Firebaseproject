ab292a5340a7ba61bce9f1f5a67b422ef422615e
Sheraliramzan <72282103+Sheraliramzan@users.noreply.github.com>
2022-11-02 15:44:42 -0700

    update

--- README.md
+++ README.md
@@ -10,25 +10,24 @@ npm run dev
 yarn dev
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+## Install packages
+yarn add firebase
 
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
+## After you added the firebase code 
+Go to firebase website and click on Authentication > get started
 
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.js`.
+Then choose the one you want 
+I will use email/password - enable the first toggle
+Go to users tab you will see all the users here that are added. We do not need to do anything here.
 
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
+## Tailwind
+yarn add -D tailwindcss postcss autoprefixer
+npx tailwindcss init -p
 
-## Learn More
+install the tailwind plugin in VS
 
-To learn more about Next.js, take a look at the following resources:
+## Dot Env Installation
+npm i dotenv
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+## VS Code Extensions:
+inline fold
--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,21 @@
-import React from "react"
-import { useState } from "react";
-import {
-  signInWithPopup,
-  GoogleAuthProvider,
-  createUserWithEmailAndPassword,
-  onAuthStateChanged,
-  signInWithEmailAndPassword,
-  signOut
-} from "firebase/auth";
-import { auth } from '../firebase/firebaseConfig';
+import React from 'react';
+import { useState } from 'react'
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { auth } from '../firebase/firebase.config';
 
- 
 export default function Home() {
+
   const [registerEmail, setRegisterEmail] = useState("");
   const [registerPassword, setRegisterPassword] = useState("");
 
   const [loginEmail, setLoginEmail] = useState("");
   const [loginPassword, setLoginPassword] = useState("");
 
-  const [user, setUser] = useState({});
-
-  const GoogleSignIn = async () => {
-    const provider = new GoogleAuthProvider();
-    
-    const authorization = auth;
-    const result = await signInWithPopup(authorization, provider);
-    
-    console.log(result);
-  }
+  const [user, setUser] = useState({})
 
   React.useEffect(() => {
     onAuthStateChanged(auth, (currentUser) => setUser(currentUser))
-  }, [])
+  }, []) 
 
   const register = async () => {
     try {
@@ -39,175 +23,80 @@ export default function Home() {
       setRegisterPassword("");
       const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);
       console.log(user);
-    } catch (error) {
-      console.log(error.message);
+    } catch(error) {
+      console.log(error.message)
     }
   }
-
 
   const login = async () => {
     try {
       const user = await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
       console.log(user);
     } catch(error) {
-      console.log(error.message);
+      console.log(error.message)
     }
   }
 
   const logout = async () => {
     await signOut(auth);
-    console.log("user logged out")
   }
 
-  const handleSubmit = event => {
-    console.log("handle submit ran");
-    event.preventDefault();
-   
-    event.target.reset();
-  }
 
   return (
     <div className="flex flex-col h-screen justify-center items-center">
-      <h3 className="text-3xl text-sky-400 font-bold underline absolute top-0 p-4">Welcome to the App</h3>
+      <h3 className="text-3xl text-sky-400 font-bold underline absolute top-0 p-4">Welcome To The App</h3>
       <div className="grid grid-cols-3 gap-4">
-        <form onSubmit={handleSubmit}>
-          <h2>Register</h2>
+        <form>
+          <h2>Register:</h2>
           <div className="border-2 p-2 m-2">
             <div className="pb-4">
               <label>Email</label>
-              <input
-                placeholder="Email..."
+              <input placeholder="Email..."
                 onChange={(event) => {
                   setRegisterEmail(event.target.value);
                 }}
                 className="flow-root m-2 p-2"
               />
               <label>Password</label>
-              <input
-                placeholder="Password..."
+              <input placeholder="Password..."
                 onChange={(event) => {
                   setRegisterPassword(event.target.value);
                 }}
                 className="flow-root m-2 p-2"
               />
             </div>
-            <button
-              type="submit"
-              className="text-blue-700 
-              hover:text-white 
-              border border-blue-700 
-              hover:bg-blue-800
-              focus:ring-4
-              focus:outline-none
-              focus:ring-blue-300
-              font-medium
-              rounded-lg
-              text-sm
-              px-5 py-2.5
-              text-center
-              mr-2
-              dark:border-blue-500
-              dark:text-blue-500
-              dark:hover:text-white
-              dark:hover:bg-blue-600
-              dark:focus:ring-blue-800
-              "
-              onClick={register}>Register User</button>
-            <button onClick={() => GoogleSignIn()}
-              className="text-blue-700 
-            hover:text-white 
-            border border-blue-700 
-            hover:bg-yellow-800
-            focus:ring-4
-            focus:outline-none
-            focus:ring-blue-300
-            font-medium
-            rounded-lg
-            text-sm
-            px-5 py-2.5
-            text-center
-            mr-2
-            dark:border-blue-500
-            dark:text-blue-500
-            dark:hover:text-white
-            dark:hover:bg-blue-600
-            dark:focus:ring-blue-800
-            "
-            >Log in with google</button>
-
+            <button type="button" className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800" onClick={register}>Register User</button>
           </div>
         </form>
+
         <form>
           <h2>Login</h2>
           <div className="border-2 p-2 m-2">
             <div className="pb-4">
               <label>Email</label>
-              <input
-                placeholder="Email..."
+              <input placeholder="Email..." 
                 onChange={(event) => {
-                  setLoginEmail(event.target.value)
-                }} 
-                className="flow-root m-2 p-2"
-                />
-              <input
-                placeholder="Password..."
-                onChange={(event) => {
-                  setLoginPassword(event.target.value)
+                  setLoginEmail(event.target.value);
                 }}
                 className="flow-root m-2 p-2"
-                />
-            </div>
-            <button onClick={login}
-            className="text-blue-700 
-            hover:text-white 
-            border border-blue-700 
-            hover:bg-yellow-800
-            focus:ring-4
-            focus:outline-none
-            focus:ring-blue-300
-            font-medium
-            rounded-lg
-            text-sm
-            px-5 py-2.5
-            text-center
-            mr-2
-            dark:border-blue-500
-            dark:text-blue-500
-            dark:hover:text-white
-            dark:hover:bg-blue-600
-            dark:focus:ring-blue-800
-            "
-            >Log in user</button>
-
+              />
+              <input placeholder="Password..."
+                onChange={(event) => {
+                  setLoginPassword(event.target.value);
+                }}
+                className="flow-root m-2 p-2"
+              /> 
+              </div>
+            <button type="button" className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800" onClick={login}> Login</button>
           </div>
         </form>
-        <div className="relative">
-          <h2>User Logged in:</h2>
-          <div className="flow-root m-2 p-2">
-            {user?.email}
-          </div>
-        <button onClick={logout}
-            className="text-blue-700 
-            hover:text-white 
-            border border-blue-700 
-            hover:bg-yellow-800
-            focus:ring-4
-            focus:outline-none
-            focus:ring-blue-300
-            font-medium
-            rounded-lg
-            text-sm
-            px-5 py-2.5
-            text-center
-            mr-2
-            dark:border-blue-500
-            dark:text-blue-500
-            dark:hover:text-white
-            dark:hover:bg-blue-600
-            dark:focus:ring-blue-800
-            "
-            >Sign Out</button>
 
+        <div className="relative">
+          <h2>User Logged In:</h2>
+            <div className="flow-root m-2 p-2">
+              {user?.email}
+            </div>
+          <button  type="button" className="absolute right-0 bottom-0 text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800" onClick={logout}> Sign Out </button>
         </div>
       </div>
     </div>
